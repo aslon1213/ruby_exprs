954151873b02ec20af129c86ca3d8f44b11f4631
aslon1213 <hamidovaslon1@gmail.com>
2022-11-30 13:01:16 +0500

    DS_project some - BFS implemented

--- python_leet_code_exercises/DS_PROJECT.py
+++ python_leet_code_exercises/DS_PROJECT.py
@@ -165,12 +165,12 @@ class hello:
 
 
 class graphOfCities:
-    """"""
+    """Grapf of cities using adjacency matrix"""
 
     number = 0
 
     def __init__(self):
-        """"""
+        """Constructor"""
         self.matrix = []
         self.cities = {}
 
@@ -181,13 +181,48 @@ class graphOfCities:
         else:
             return False
 
-    def path_exists(self):
-        # BFS or DFS
-        pass
+    def path_exists(self, city_from, city_to):
+        # if city has no neigbor then return False otherwise return True
+        if (
+            sum(self.matrix[city_from.number]) == 0
+            or sum(self.matrix[city_to.number]) == 0
+        ):
+            return False
+        else:
+            return self.BFS(city_from, city_to)
 
-    def BFS(self, args):
-        queue = []
-        pass
+    def BFS(self, city_from, city_to):
+        queue = []  # queue
+        cities_ = list(self.cities.values())  # City objects
+        status_list = [1 for i in range(len(self.matrix))]  # status of cities
+        # 1 - unvisited
+        # 2 - visting
+        # 3 - visited
+        queue.append(city_from.number)
+        status_list[city_from.number] = 2
+        traversal = []  # traversal of grapf by BFS
+        predecessors = {}  # to keep track of predecessors of cities while traversing
+        while queue:
+            s = queue.pop(0)
+            traversal.append(s)
+            status_list[s] = 3
+            for c in cities_[s][1].neighbours:
+                if status_list[c.number] == 1:
+                    queue.append(c.number)
+                    predecessors[c.number] = s
+                    status_list[c.number] = 2
+
+        # getting the easiest track of city_from to city_to
+        output = []
+        a = city_to.number
+        b = predecessors[a]
+        output.append(a)
+        while b != city_from.number:
+            a = predecessors[a]
+            b = predecessors[a]
+            output.append(a)
+        output.append(city_from.number)
+        return output
 
     def DFS(self, args):
         pass
@@ -195,15 +230,19 @@ class graphOfCities:
     def exists(self, city):
         """To check whether city(node) exists in the graph"""
         cities = []
+        # add City objects from self.cities hastable to cities array
         for k in self.cities.values():
             cities.append(k[1])
-        # print(k.name for k in cities)
+
+        # iterate through the cities array until its found
         if city in cities:
             return True
         else:
             return False
 
     def create_city(self, city_name):
+        """add city to graph - no connection/neighbors
+        just a city separated"""
         a = City(city_name, self.number)
         self.number += 1
         for i in self.matrix:
@@ -213,27 +252,34 @@ class graphOfCities:
         return a
 
     def get_routes_by_numbers(self):
-        cities = self.cities.values()
-        output = []
-        matrixes = self.matrix
+        cities = self.cities.values()  # City objects array
+        output = []  # array to be returned
+        matrixes = self.matrix  # adjacency matrix of graph
         length = len(matrixes)
         for i in range(length):
             for j in range(length):
-                if matrixes[i][j] == 1:
-                    a = [i, j]
-                    b = [j, i]
+                if matrixes[i][j] == 1:  # if cities are neigbors
+                    a = [i, j]  # contruct array representing of their numbers
+                    b = [j, i]  # meaning that they are neigbors
                     if a not in output and b not in output:
-                        output.append(a)
+                        output.append(a)  # add that array to output
 
         print(output)
         return output
 
     def add_city_neighbors(self, city_1, city_2, distance):
-        self.matrix[city_1.number][city_2.number] = 1
-        self.matrix[city_2.number][city_1.number] = 1
+        """make two cities(city_1 and city_2) neighbors"""
+        self.matrix[city_1.number][
+            city_2.number
+        ] = 1  # set ith row jth column of matrix to 1 as city_1 has neighbor city_2
+        self.matrix[city_2.number][
+            city_1.number
+        ] = 1  # set jth row ith column of matrix to 1 as city_2 has neighbor city_1
         city_1.add_city_neighbor(city_2, distance)
 
     def __str__(self):
+
+        """string converter function"""
         output = "        "
         cities_ = list(self.cities.keys())
         for c in cities_:
@@ -253,12 +299,13 @@ class graphOfCities:
 
 class City:
     def __init__(self, name, number):
-        self.neighbours = []
-        self.distances = []
-        self.name = name
-        self.number = number
+        self.neighbours = []  # neighbors of this city
+        self.distances = []  # distances from the this city to its neighbors
+        self.name = name  # name of the city - String
+        self.number = number  # number of the city is the graph
 
     def add_city_neighbor(self, ng, distance):
+        """Add city to neighbors list(array)"""
         if isinstance(ng, City):
             self.neighbours.append(ng)
             self.distances.append(distance)
@@ -266,7 +313,10 @@ class City:
             ng.distances.append(distance)
 
     @staticmethod
-    def construct_paths(self):
+    def construct_path():
+        """contruct path from this city to city given
+        using DFS or BFS
+        """
         pass
 
     def __str__(self):
@@ -325,7 +375,9 @@ e = a.create_city("Fargona")
 c = a.create_city("Bukhara")
 g = a.create_city("Qarshi")
 f = a.create_city("Xorazm")
-f = a.create_city("Xorazm")
+r = a.create_city("XXXXXX")
+t = a.create_city("YYYYYYY")
+u = a.create_city("X")
 
 
 # create connection beetween cities
@@ -336,11 +388,17 @@ a.add_city_neighbors(e, d, 20)
 a.add_city_neighbors(c, f, 80)
 a.add_city_neighbors(b, g, 100)
 
+a.add_city_neighbors(e, r, 200)
+a.add_city_neighbors(r, t, 100)
+a.add_city_neighbors(t, u, 100)
+
+
 # print
 # print(b)
 # print(c)
 # print(d)
 # print(a)
-print(a.exists(b))
-y = a.get_routes_by_numbers()
-print(minimumFuelCost(roads=y, seats=5))
+# print(a.exists(b))
+# y = a.get_routes_by_numbers()
+# print(minimumFuelCost(roads=y, seats=5))
+print(a.BFS(b, f))
